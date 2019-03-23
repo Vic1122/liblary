844c0196b2b61ca528cd6369dee51de7dff2b695
vic1122 <wolski.wiktor@gmail.com>
2019-03-23 11:07:00 +0100

    dodanie projektu

---
+++ src/test/java/com/wsiz/liblary/LiblaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wsiz.liblary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiblaryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
